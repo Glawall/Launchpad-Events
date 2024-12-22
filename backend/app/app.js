906d74c2a048cc01e0f1de241da10074fffcd777
Glawall <glawall217@gmail.com>
2024-12-22 14:36:36 +0000

    added cors

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,8 +1,11 @@
 import router from "./routes";
 import express from "express";
 import { errorHandler } from "./middleware/errorHandler";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
