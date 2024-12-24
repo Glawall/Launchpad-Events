54a6304118b8b5af0830b56faa701b844f082bc0
Glawall <glawall217@gmail.com>
2024-12-24 15:46:18 +0000

    Deploy frontend to GitHub Pages

--- community-events/vite.config.js
+++ community-events/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+
+export default {
   plugins: [react()],
-  base: "/Launchpad-Events/",
-});
+  base: "/Launchpad-Events/", // Set this to your GitHub repo name
+  build: {
+    outDir: "../docs", // Output the build files into the /docs folder
+  },
+};
