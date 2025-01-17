e9fd39590fd20eb422cca74ed21fe91003a27b1c
Glawall <glawall217@gmail.com>
2025-01-17 13:08:42 +0000

    sorting basepath

--- Launchpad-Events/src/main.jsx
+++ Launchpad-Events/src/main.jsx
@@ -5,7 +5,8 @@ import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 import App from "./App";
 
-const basePath = process.env.VITE_BASE_PATH || "";
+const basePath = process.env.VITE_BASE_PATH;
+console.log(VITE_BASE_PATH);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
