b68c3e0e82e391377d2506ca11497fe0d5a4464b
Glawall <glawall217@gmail.com>
2025-01-17 11:02:21 +0000

    sorting out file structure

--- community-events/vite.config.js
+++ community-events/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH, // Use the base path from the environment variables
+  base: process.env.VITE_BASE_PATH,
   build: {
-    outDir: "../docs", // Output the build files into the /docs folder (for GitHub Pages)
+    outDir: "dist",
   },
 });
