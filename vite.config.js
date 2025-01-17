531080f3995a182af3ffa6147cf87de88114bd03
Glawall <glawall217@gmail.com>
2025-01-17 10:45:59 +0000

    Fix base URL in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   base: "/Launchpad-Events/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
