59fba95ddcc3aa09f434a616bef927608431aeda
Glawall <glawall217@gmail.com>
2025-02-10 13:20:18 +0000

    calendar update

--- Launchpad-Events/src/utils/google-calendar.js
+++ Launchpad-Events/src/utils/google-calendar.js
@@ -5,23 +5,34 @@ const SCOPES = "https://www.googleapis.com/auth/calendar.events";
 let tokenClient;
 
 export const initGoogleApi = async () => {
-  await new Promise((resolve) => gapi.load("client:auth2", resolve));
+  await new Promise((resolve) => gapi.load("client", resolve));
+
   await gapi.client.init({
     apiKey: GOOGLE_API_KEY,
-    clientId: GOOGLE_CLIENT_ID,
-    scope: SCOPES,
     discoveryDocs: [
       "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
     ],
   });
+
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: GOOGLE_CLIENT_ID,
+    scope: SCOPES,
+    prompt: "consent",
+  });
 };
 
 export const addEventToCalendar = async (event) => {
-  if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
-    await gapi.auth2.getAuthInstance().signIn({
-      prompt: "consent",
-    });
-  }
+  await new Promise((resolve, reject) => {
+    try {
+      tokenClient.callback = async (resp) => {
+        if (resp.error) reject(resp);
+        resolve(resp);
+      };
+      tokenClient.requestAccessToken();
+    } catch (err) {
+      reject(err);
+    }
+  });
 
   const calendarEvent = {
     summary: event.title,
